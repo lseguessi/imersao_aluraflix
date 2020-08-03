62b58be64efa2afce4dd37fb2bc2bf508c739614
lseguessi <lucas.seguessi@gmail.com>
2020-08-02 22:51:55 -0300

    eslint add to project

---
+++ src/components/FormField/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+  return (
+    <div>
+      <label
+        htmlFor={fieldId}
+      >
+        {label}
+        :
+        <input
+          id={fieldId}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+}
+export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,116 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
-function CadastroCategoria(){
-    return (
-        <PageDefault>
-            <h1>Cadasto de Categoria</h1>
+function CadastroCategoria() {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+    const [categorias, setCategoria] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
 
-                
 
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
+    function setValue(chave, valor){
+        setValues({
+            ...values,
+            [chave]: valor,
+        })
+    }   
+
+    function handlerChange(infosDoEvento){
+        setValue(
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+            // getAttribute('name'), 
+            // value
+        );
+    }
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form onSubmit={ function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategoria([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
+      }}>
+
+        <FormField 
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange={handlerChange}
+        />
+
+
+
+        <FormField 
+            label="Descrição"
+            type="???"
+            name="descricao"
+            value={values.descricao}
+            onChange={handlerChange}
+        />
+        {/* <div>
+            <label>
+            Descrição:
+            <textarea
+                type="text"
+                value={values.descricao}
+                name="descricao"
+                onChange={handlerChange}
+            />
+            </label>
+        </div> */}
+
+          <FormField 
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handlerChange}
+        />
+          {/* <div>
+            <label>
+            Cor:
+            <input
+                type="color"
+                value={values.cor}
+                name="cor"
+                onChange={handlerChange}
+            />
+            </label>
+          </div> */}
+
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+          {categorias.map((categoria, indice) => {
+              return <li key={`${categoria}`+indice}>
+                  {categoria.nome}
+              </li>
+          })}
+      </ul>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
